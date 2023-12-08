c91169e82d530af5c00ca62aadf72f1e889e9673
adrien <dela1102@usherbrooke.ca>
2023-12-07 19:50:14 -0500

    scroll page historique

--- app/src/main/java/com/example/projetsessionandroid/ui/screen/historicScreen/HistoricScreenView.kt
+++ app/src/main/java/com/example/projetsessionandroid/ui/screen/historicScreen/HistoricScreenView.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -49,6 +51,7 @@ fun HistoricScreenView(navController: NavHostController, user: User, historic : 
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
 
         ) {
             // Affichage des dons ou des commands en fonction de l'historique souhaité
